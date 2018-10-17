888b2cd9cdb5a219a1c7f8f4311d35e827d9e75f
yuki <feather2150@gmail.com>
2018-10-17 22:59:58 +0800

    Add files via upload

---
+++ CA/all.js
@@ -0,0 +1,20 @@
+$('.main_content').hide();
+$( ".title" ).click(function() {
+  
+  $('.main_content').slideToggle();
+  
+});
+
+$('.main_content2').hide();
+$( ".title2" ).click(function() {
+  
+  $('.main_content2').slideToggle();
+  
+});
+
+$('.main_content3').hide();
+$( ".title3" ).click(function() {
+  
+  $('.main_content3').slideToggle();
+  
+});
