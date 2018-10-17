f43b3ccdc9ae968c277db303997fd187020a70c1
yuki <feather2150@gmail.com>
2018-10-17 23:04:15 +0800

    Add files via upload

---
+++ CI/all.js
@@ -0,0 +1,34 @@
+$('#introduce').hide();
+$( ".title" ).click(function() {
+  
+  $(this).next().slideToggle();
+  
+});
+
+$('#introduce2').hide();
+$( ".title2" ).click(function() {
+  
+  $(this).next().slideToggle();
+  
+});
+
+$('#introduce3').hide();
+$( ".title3" ).click(function() {
+  
+   $(this).next().slideToggle();
+  
+});
+
+$('#introduce4').hide();
+$( ".title4" ).click(function() {
+  
+   $(this).next().slideToggle();
+  
+});
+
+$('.career').hide();
+$( ".ex" ).click(function() {
+  
+   $(this).next().slideToggle();
+  
+});
